82edd72899a8fbea409b14bbc6bbab47a4f86862
Admadma <admadmaa77@gmail.com>
2022-01-08 06:08:13 +0100

    Basic game UI and very basic logic added

---
+++ torpedo/Models/GameModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace torpedo.Models
+{
+    public class GameModel
+    {
+        
+        private int _gameWidth = 3;
+        private int _gameHeight = 3;
+        //hit mindkét képernyő(player/opponent) ugyan azt ábrázolja majd: egy eltalált hajó
+        public enum fieldState { Untouched, Hit, Miss };
+        //public fieldState[][] fieldStates { get; set; }
+        public fieldState[,] fieldStates { get; set; }
+
+        public GameModel()
+        {
+            fieldStates = new fieldState[_gameWidth, _gameHeight];
+
+            for (int i = 0; i < _gameWidth; i++)
+            {
+                for(int j = 0; j < _gameHeight; j++)
+                {
+                    fieldStates[i,j] = fieldState.Untouched;
+                }
+            }
+
+            fieldStates[0, 0] = fieldState.Hit;
+        }
+
+        public fieldState getMyFieldState(Position position)
+        {
+            return fieldStates[position.X, position.Y];
+        }
+
+        public void asd()
+        {
+            
+        }
+
+    }
+}
---
+++ torpedo/ViewModels/PlayerBoardViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using torpedo.Models;
+
+namespace torpedo.ViewModels
+{
+    public class PlayerBoardViewModel
+    {
+        Models.GameModel gameModel;
+
+        public PlayerBoardViewModel()
+        {
+            gameModel = new GameModel();
+        }
+
+ 
+    }
+}
