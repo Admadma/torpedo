cb540849359448fdd2b0d36f8c8caeefae4c1cc6
HoriiS <horvathpisti.26@gmail.com>
2022-01-13 17:17:47 +0100

    Add pvp gameover screen

---
+++ torpedo/PlayerVsPlayerGO.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml;
+using System.Xml.Linq;
+using System.Data;
+using System.IO;
+
+namespace torpedo
+{
+    /// <summary>
+    /// Interaction logic for PlayerVsPlayerGO.xaml
+    /// </summary>
+    public partial class PlayerVsPlayerGO : Window
+    {
+        string Playerone = "playerone";
+        string Playertwo = "playertwo";
+        string Winner = "winner";
+        public PlayerVsPlayerGO()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (File.Exists("Scores.xml") == false)
+            {
+                XmlWriterSettings xmlWriterSettings = new XmlWriterSettings();
+                xmlWriterSettings.Indent = true;
+                xmlWriterSettings.NewLineOnAttributes = true;
+                using (XmlWriter xmlWriter = XmlWriter.Create("Scores.xml", xmlWriterSettings))
+                {
+                    xmlWriter.WriteStartDocument();
+                    xmlWriter.WriteStartElement("Scores");
+                    xmlWriter.WriteStartElement("Game");
+                    xmlWriter.WriteElementString("PlayerOne", Playerone);
+                    xmlWriter.WriteElementString("PlayerTwo", Playertwo);
+                    xmlWriter.WriteElementString("Result", Winner + " Won");
+                    xmlWriter.WriteEndElement();
+                    xmlWriter.WriteEndElement();
+                    xmlWriter.WriteEndDocument();
+                    xmlWriter.Flush();
+                    xmlWriter.Close();
+                }
+            }
+            else
+            {
+                XDocument xDocument = XDocument.Load("Scores.xml");
+                XElement root = xDocument.Element("Scores");
+                IEnumerable<XElement> rows = root.Descendants("Game");
+                XElement firstRow = rows.First();
+                firstRow.AddBeforeSelf(
+                   new XElement("Game",
+                   new XElement("PlayerOne", Playerone),
+                   new XElement("PlayerTwo", Playertwo),
+                   new XElement("Result", Winner + " Won")));
+                xDocument.Save("Scores.xml");
+            }
+        }
+    }
+}
