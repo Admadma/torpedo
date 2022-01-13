295513a1d2c02f9aa84cf070ba7c0a94a28015cf
HoriiS <horvathpisti.26@gmail.com>
2022-01-13 19:20:46 +0100

    added name validation to pvc

--- torpedo/PlayerVsComputer.xaml.cs
+++ torpedo/PlayerVsComputer.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -26,6 +27,12 @@ namespace torpedo
 
         private void onPvcPlay(object sender, RoutedEventArgs e)
         {
+            Regex regex = new Regex(@"[^a-zA-Z0-9$]");
+            if (regex.IsMatch(Player1c.Text) || string.IsNullOrWhiteSpace(Player1c.Text))
+            {
+                MessageBox.Show("Player1 name is invalid");
+                return;
+            }
             PlayerVsComputerGame pvcgWindow = new PlayerVsComputerGame();
             pvcgWindow.Show();
             this.Close();
